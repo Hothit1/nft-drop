353e77585e8ebf7b1bf06bec97bf64ed493eea13
Hothit1 <107194459+Hothit1@users.noreply.github.com>
2023-06-17 22:40:32 +0700

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,12 +2,12 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0xDbcc1F36Adc4a6fFFDef09Dd9Ca87Ed59a046ad0";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
 // Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
-export const chainConst = "mumbai";
+export const chainConst = "binance";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "white";
